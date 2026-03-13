82f2d3faa6cb9a2054f6e2161142cfbe3cb2e25d
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-03-13 17:17:40 -0400

    WK 9 In class work!

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControllerInput : MonoBehaviour
+{
+    public float speed;
+    public Vector2 directionalInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += (Vector3)directionalInput * speed * Time.deltaTime;
+
+    }
+
+    public void OnMove (InputAction.CallbackContext context)
+    {
+        directionalInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            Debug.Log("Attack Time(" + context.phase + ")");
+            //string.x = "Cat" + "" + "Dog"; // = "Cat Dog";
+        }
+
+
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+
+        Vector2 mousePosition = context.ReadValue<Vector2>();
+        Vector2 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Debug.Log("OnPoint" + worldMousePosition);
+    }
+
+}
