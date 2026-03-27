065959575810f249f0282c4b066dae30b661e699
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-03-27 16:12:27 -0400

    Player Move

---
+++ Assets/Knight Scripts/Knight.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Knight : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float speed;
+
+    public float xMovement;
+
+    public Animator knightAnimator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(xMovement, 0f, 0f) * speed * Time.deltaTime;
+    }
+
+    public void OnFootstep()
+    {
+        Debug.Log("Footstep");
+
+        audioSource.Play();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 moveDirection = context.ReadValue<Vector2>();
+        xMovement = moveDirection.x;
+
+        bool isRunning = xMovement != 0f;
+
+        knightAnimator.SetBool("isRunning", isRunning);
+    }
+
+}
