612de8909a50555c1f02f2953f50d429fa2f6b63
Robert Caballero Gomez <caballro@shernet.sheridancollege.ca>
2026-04-10 17:18:21 -0400

    First Coding Gym Done!

--- Assets/Scripts/LocalMultiPlayer.cs
+++ Assets/Scripts/LocalMultiPlayer.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,15 @@ public class LocalMultiPlayer : MonoBehaviour
     public Vector2 moveDirection;
     public float moveSpeed;
     public LocalMultiplayerManager manager;
+
+    public Vector3 playerScale;
+    public AnimationCurve scaleCurve;
+
+    float progress;
+    float duration = 1;
+    float time;
+
+    Coroutine attacking;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +28,8 @@ public class LocalMultiPlayer : MonoBehaviour
     void Update()
     {
         transform.position += (Vector3)moveDirection * moveSpeed * Time.deltaTime;
+
+        //Debug.Log(playerScale);
     }
 
 
@@ -29,16 +41,54 @@ public class LocalMultiPlayer : MonoBehaviour
 
     public void OnAttack (InputAction.CallbackContext context)
     {
+        
 
         if (context.performed)
         {
 
+            if (attacking != null)
+            {
+                StopCoroutine(attacking); 
+            }
+
             PlayerInput playerInput = GetComponent<PlayerInput>();
             manager.TryAttack(playerInput);
+
+            attacking = StartCoroutine(Attacking());
+
+        
         }
 
         Debug.Log("Attack: " + context.phase);
 
+    }
+
+    private IEnumerator Attacking()
+    {
+       
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+
+            progress = scaleCurve.Evaluate(time);
+
+            playerScale = new Vector3(1, progress, 1);
+
+            transform.localScale = playerScale;
+
+            if (time > duration)
+            {
+                Debug.Log("stop");
+
+                StopCoroutine(attacking);
+
+                time = 0;
+               
+            }
+
+            yield return null;
+
+        }
     }
 
 }
